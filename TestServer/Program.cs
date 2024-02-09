3b0089c6b5bed70484ef580b3cc5e2ad97d643ca
M Thomason <matt@warpedcore.net>
2024-02-09 22:24:58 +0000

    Move server into its own project

---
+++ TestServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using GenHTTP.Api.Content.IO;
+using GenHTTP.Api.Infrastructure;
+using GenHTTP.Engine;
+using GenHTTP.Modules.IO.Zipfile;
+using GenHTTP.Modules.Practices;
+using GenHTTP.Modules.StaticWebsites;
+using GenHTTP.Modules.StaticWebsites.Provider;
+using System.IO.Compression;
+
+//create zipfile of Test Site
+if (File.Exists("testsite.zip"))
+{
+    File.Delete("testsite.zip");
+}
+ZipFile.CreateFromDirectory("TestSite", "testsite.zip");
+
+//load resource tree from zip
+IBuilder<IResourceTree> content = ResourceTree.FromZip("test.zip");
+
+StaticWebsiteBuilder? app = StaticWebsite.From(content);
+
+Host.Create()
+    .Console()
+    .Defaults()
+    .Handler(app)
+    .Run();
+
+Console.WriteLine("Press any key to terminate");
+Console.ReadKey();
